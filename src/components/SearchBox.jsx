048c6ed02d8ff41a85179e93df6e0c84a512b279
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 17:15:28 +0330

    create SearchBox component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import SearchResult from "./SearchResult";
+
+function SearchBox() {
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="mt-40">
+      <input
+        className="bg-zinc-700 w-72 p-2 rounded-lg focus:outline-none"
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={searchHandler}
+      />
+      {search && <SearchResult search={search} />}
+    </div>
+  );
+}
+
+export default SearchBox;
