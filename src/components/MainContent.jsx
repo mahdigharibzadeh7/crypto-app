2ed94ea2f1173a41972724515aec2b80e6d44c15
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 21:04:35 +0330

    add currency state and handler and component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import SearchBox from "./SearchBox";
 import Table from "../layouts/Table";
 import Pagination from "./Pagination";
+import Currency from "./Currency";
 
 function MainContent() {
   const [coins, setCoins] = useState([]);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
 
   const pagesLength = coins.length;
 
@@ -13,10 +15,22 @@ function MainContent() {
     setPage(e.target.textContent);
   };
 
+  const currencyHandler = (e) => {
+    setCurrency(e.target.value);
+  };
+
   return (
     <div>
-      <SearchBox />
-      <Table coins={coins} setCoins={setCoins} page={page} />
+      <div className="flex items-center mt-40 gap-x-5">
+        <SearchBox />
+        <Currency currencyHandler={currencyHandler} />
+      </div>
+      <Table
+        coins={coins}
+        setCoins={setCoins}
+        page={page}
+        currency={currency}
+      />
       <Pagination
         pagesLength={pagesLength}
         paginationHandler={paginationHandler}
