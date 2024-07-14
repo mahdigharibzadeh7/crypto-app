c895c87269be1f1543100012903f9760276e18c7
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 15:14:29 +0330

    first commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="text-center bg-blue-500 rounded-lg p-5 mt-2 font-bold">
+      Designed By | Mahdi Gharibzadeh ❤️
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="flex justify-between items-center bg-blue-500 rounded-lg p-5 mt-2 font-bold">
+      <div className="text-3xl">Crypto App</div>
+      <div className="text-lg">
+        Developer | <span className="text-blue-900">Mahdi Gharibzadeh</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
