829f578a561e6ed98de56492b1af39d4e3dd5924
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 21:05:10 +0330

    add margin top and bottom

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="text-center bg-blue-500 rounded-lg p-5 mt-2 font-bold">
+    <footer className="text-center bg-blue-500 rounded-lg p-5 mt-7 mb-3 font-bold">
       Designed By | Mahdi Gharibzadeh ❤️
     </footer>
   );
