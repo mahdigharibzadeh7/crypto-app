bf9aa91d4fd0fa5e10090a14b4aa70d401140e3d
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 20:39:24 +0330

    add Pagination component ans some state

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import SearchBox from "./SearchBox";
 import Table from "../layouts/Table";
+import Pagination from "./Pagination";
 
 function MainContent() {
+  const [coins, setCoins] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const pagesLength = coins.length;
+
+  const paginationHandler = (e) => {
+    setPage(e.target.textContent);
+  };
+
   return (
     <div>
       <SearchBox />
-      <Table />
+      <Table coins={coins} setCoins={setCoins} page={page} />
+      <Pagination
+        pagesLength={pagesLength}
+        paginationHandler={paginationHandler}
+      />
     </div>
   );
 }
