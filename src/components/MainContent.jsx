b2e175cc4867a3f69758d76845958eb987d07e0c
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-14 17:15:06 +0330

    add SearchBox component

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBox from "./SearchBox";
+
+function MainContent() {
+  return (
+    <div>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default MainContent;
