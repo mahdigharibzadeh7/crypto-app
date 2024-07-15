0c050075428ef163cdc163903817a140c1948989
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 14:22:36 +0330

    add CoinTableRow component into Table for component warn for table tag

--- src/layouts/Table.jsx
+++ src/layouts/Table.jsx
@@ -1,6 +1,24 @@
-import CoinTableRow from "../components/CoinTableRow";
+import { useEffect } from "react";
+import { getCoinsList } from "../services/cryptoApi";
+// import Lottie from "react-lottie-player";
+// import loader from "../assets/loader.json";
+import chart_up from "../assets/chart-up.svg";
+import chart_down from "../assets/chart-down.svg";
 
 function Table({ coins, setCoins, page, currency }) {
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const response = await fetch(getCoinsList(currency, page));
+        const json = await response.json();
+        setCoins(json);
+        pageLenth = coins.length;
+      } catch (err) {
+        console.log(err.name);
+      }
+    };
+    fetchCoins();
+  }, [page, currency]);
   return (
     <table className="w-full mt-5 border-separate border-spacing-y-7">
       <thead>
@@ -13,14 +31,50 @@ function Table({ coins, setCoins, page, currency }) {
           <th></th>
         </tr>
       </thead>
-      <CoinTableRow
-        coins={coins}
-        setCoins={setCoins}
-        page={page}
-        currency={currency}
-      />
+      <tbody>
+        {coins.map((coin) => (
+          <TableRow key={coin.id} coin={coin} currency={currency} />
+        ))}
+      </tbody>
     </table>
   );
 }
 
 export default Table;
+
+const TableRow = ({ coin, currency }) => {
+  return (
+    <tr>
+      <td className="flex items-center gap-x-3">
+        <img className="w-7 rounded-full" src={coin.image} alt={coin.name} />
+        <span>{coin.symbol.toUpperCase()}</span>
+      </td>
+      <td>{coin.name}</td>
+      <td>
+        {currency === "usd" ? "$" : currency === "eur" ? "€" : "¥"}
+        {coin.current_price.toLocaleString()}
+      </td>
+      <td
+        className={`${
+          coin.price_change_percentage_24h > 0
+            ? "text-green-600"
+            : "text-red-600"
+        }`}
+      >
+        {coin.price_change_percentage_24h.toFixed(2)}%
+      </td>
+      <td>
+        {currency === "usd" ? "$" : currency === "eur" ? "€" : "¥"}
+        {coin.total_volume.toLocaleString()}
+      </td>
+      <td>
+        <img
+          src={`${
+            coin.price_change_percentage_24h > 0 ? chart_up : chart_down
+          }`}
+          alt={`${coin.name} chart`}
+        />
+      </td>
+    </tr>
+  );
+};
