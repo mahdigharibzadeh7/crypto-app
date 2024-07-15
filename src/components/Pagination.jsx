186a667c70ba853d61598790fc154bacd686ebe8
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 17:03:00 +0330

    edit Pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,20 +1,69 @@
-function Pagination({ pagesLength, paginationHandler }) {
-  const paginationNumber = [];
-  for (let index = 1; index <= pagesLength; index++) {
-    paginationNumber.push(index);
-  }
+import PaginateButton from "./PaginateButton";
+
+function Pagination({ currentNumber, setCurrentNumber, paginationHandler }) {
+  const ButtonNumber = [1, 2, 3, 4, 5, 16, 17, 18, 19, 20];
+
+  const prevHandler = () => {
+    if (currentNumber > 1) setCurrentNumber((num) => num - 1);
+  };
+  const nextHandler = () => {
+    if (currentNumber < 20) setCurrentNumber((num) => num + 1);
+  };
 
   return (
     <div className="w-full mx-5 flex justify-center gap-x-2">
-      {paginationNumber.map((pn) => (
-        <button
-          key={pn}
-          className="border px-3 py-1 rounded-lg hover:bg-blue-500"
-          onClick={paginationHandler}
-        >
-          {pn}
-        </button>
+      <button
+        className={`border px-3 py-1 rounded-lg bg-blue-500 ${
+          currentNumber === 1 &&
+          "bg-zinc-500 text-zinc-300 hover:cursor-not-allowed"
+        }`}
+        onClick={prevHandler}
+      >
+        Previous
+      </button>
+      {ButtonNumber.slice(0, 5).map((num) => (
+        <PaginateButton
+          key={num}
+          number={num}
+          currentNumber={currentNumber}
+          setCurrentNumber={setCurrentNumber}
+          paginationHandler={paginationHandler}
+        />
       ))}
+      {
+        <>
+          {currentNumber > 5 && currentNumber < 16 && (
+            <>
+              <span>...</span>
+              <PaginateButton
+                number={currentNumber}
+                currentNumber={currentNumber}
+                setCurrentNumber={setCurrentNumber}
+                paginationHandler={paginationHandler}
+              />
+            </>
+          )}
+        </>
+      }
+      <span>...</span>
+      {ButtonNumber.slice(5).map((num) => (
+        <PaginateButton
+          key={num}
+          number={num}
+          currentNumber={currentNumber}
+          setCurrentNumber={setCurrentNumber}
+          paginationHandler={paginationHandler}
+        />
+      ))}
+      <button
+        className={`border px-3 py-1 rounded-lg bg-blue-500 ${
+          currentNumber === 20 &&
+          "bg-zinc-500 text-zinc-300 hover:cursor-not-allowed"
+        }`}
+        onClick={nextHandler}
+      >
+        Next
+      </button>
     </div>
   );
 }
