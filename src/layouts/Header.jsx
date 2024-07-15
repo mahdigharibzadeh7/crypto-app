82b13c96e26b1961a8de52bca174ee053556c6b7
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 22:09:53 +0330

    add margin x

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="flex justify-between items-center bg-blue-500 rounded-lg p-5 mt-2 font-bold">
+    <header className="flex justify-between items-center bg-blue-500 rounded-lg p-5 mt-2 font-bold mx-40">
       <div className="text-3xl">Crypto App</div>
       <div className="text-lg">
         Developer | <span className="text-blue-900">Mahdi Gharibzadeh</span>
