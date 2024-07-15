6ff3f5cffc3fc2ade0612b12d5d467c0bcb1d005
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 12:21:15 +0330

    add thead and tbody

--- src/layouts/Table.jsx
+++ src/layouts/Table.jsx
@@ -3,14 +3,16 @@ import CoinTableRow from "../components/CoinTableRow";
 function Table({ coins, setCoins, page, currency }) {
   return (
     <table className="w-full mt-5 border-separate border-spacing-y-7">
-      <tr className="text-left font-bold text-xl">
-        <th>Coin</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>24h</th>
-        <th>Total Volume</th>
-        <th>chart</th>
-      </tr>
+      <thead>
+        <tr className="text-left font-bold text-xl">
+          <th>Coin</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>24h</th>
+          <th>Total Volume</th>
+          <th>chart</th>
+        </tr>
+      </thead>
       <CoinTableRow
         coins={coins}
         setCoins={setCoins}
