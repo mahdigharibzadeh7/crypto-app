dcf113c594aa490cdcc20ff00bae5b0b15f7dca7
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 22:05:58 +0330

    add Modal component and modal state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import MainContent from "./components/MainContent";
+import Modal from "./components/Modal";
 import Layout from "./layouts/Layout";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <Layout>
-      <MainContent />
-    </Layout>
+    <>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <Layout>
+        <MainContent setShowModal={setShowModal} />
+      </Layout>
+    </>
   );
 }
 
