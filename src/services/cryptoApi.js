ae9294277b40753f6337948b6ca29286507853c3
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-15 19:07:11 +0330

    edit getSearchList url

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,12 +1,10 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-4iqXxLnATz5uu3DL2JiA245v";
 
-const getCoinsList = (currency, page) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&per_page=20&page=${page}&x-cg-demo-api-key=${API_KEY}`;
-};
+const getCoinsList = (currency, page) =>
+  `${BASE_URL}/coins/markets?vs_currency=${currency}&per_page=20&page=${page}&x-cg-demo-api-key=${API_KEY}`;
 
-const getSearchList = (currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&x-cg-demo-api-key=${API_KEY}`;
-};
+const getSearchList = (query) =>
+  `${BASE_URL}/search?query=${query}&x-cg-demo-api-key=${API_KEY}`;
 
 export { getCoinsList, getSearchList };
