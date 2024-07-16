e5ac59a920cf41e1c6735bff3b02d26a74164452
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-16 17:28:22 +0330

    fix some detail

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,20 @@
-function Modal({ showModal, setShowModal }) {
+import Chart from "./Chart";
+
+function Modal({ setShowModal, chartData, chartType, setChartType }) {
   const closeHandler = () => {
     setShowModal((modal) => !modal);
+    setChartType("prices");
   };
   return (
-    <div
-      className={`w-full ${
-        showModal && "fixed"
-      }  min-h-[100vh] top-0 left-0 backdrop-blur-sm z-10 ${!showModal && "hidden"}`}
-    >
+    <div className="w-full fixed min-h-[100vh] top-0 left-0 backdrop-blur-sm z-10">
       <button className="bg-red-500 rounded-lg p-2 m-10" onClick={closeHandler}>
         ✖
       </button>
-      <div className="flex justify-center items-center max-w-full min-h-[100vh]">
-        this is chart
-      </div>
+      <Chart
+        chartData={chartData}
+        chartType={chartType}
+        setChartType={setChartType}
+      />
     </div>
   );
 }
